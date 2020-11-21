09b8e68cd9c42edb3ba0261d7a09cc20eed2b9e1
github username <github email address>
2020-11-21 11:10:08 -0600

    Submitted by Kirsty B.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/(\.|\!|\?)/)
+    sentences.count do | each_sentence |
+      each_sentence.length > 1
+    end
   end
+
 end
