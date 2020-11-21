a04f6da0b117cf0d24f88b4bf434d26562c0b26a
github username <github email address>
2020-11-21 11:09:34 -0600

    commit tree aa1d6243fb17b7df116081ebc5934e5ac7890099

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #binding.pry
+    sentences = self.split(/(\.|\!|\?)/)
+    sentences.count do | each_sentence |
+      each_sentence.length > 1
+    end
   end
+
 end
